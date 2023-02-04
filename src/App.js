32c00da46a8cb14f54c332deee7aa22e3a5b13ac
Michelle Lo <lomichelle42@gmail.com>
2023-02-04 16:14:26 +0000

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Projects from './routes/Projects';
+
 function App() {
+
+  const routes = (
+    <Routes className="appBody">
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +29,38 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {routes}
     </div>
   );
 }
 
 export default App;
+
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import Home from './routes/Home';
+// import Projects from './routes/Projects';
+
+// class App extends React.Component {
+
+//   render() {
+//     const { loading, user } = this.state;
+
+//     const routes = (
+//       <Routes className="appBody">
+//         <Route path="/" element={<Home />} />
+//         <Route path="/projects" element={<Projects />} />
+//       </Routes>
+//     )
+
+//     return (
+//       <div>
+//       <h1>hello</h1>
+//         {/* {routes} */}
+//         </div>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ src/routes/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Projects extends React.Component {
+
+  render() {   
+    return (
+      <div className="Projects">
+        <h1> Projects </h1>
+      </div>
+    );
+  }
+}
+
+export default Projects;
